419bb3ebd778be251ef6120676bf2c69e2a4a0d5
nichathong <nicha.fn@gmail.com>
2023-03-30 13:49:16 -0700

    test

---
+++ init-db.js
@@ -0,0 +1,13 @@
+const db = require('./db')
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS products (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      description TEXT,
+      price REAL,
+      image_url TEXT
+    )
+  `);
+});
