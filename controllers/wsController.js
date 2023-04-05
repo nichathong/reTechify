5614a589263097723d13846e33f96308bb1e1c70
nichathong <nicha.fn@gmail.com>
2023-04-05 16:27:07 -0700

    Initialized websocket

--- controllers/wsController.js
+++ controllers/wsController.js
@@ -1,2 +1,20 @@
 const WebSocket = require('ws');
 
+const wss = new WebSocket.Server({ noServer: true });
+
+const handleConnection = (ws, req) => {
+    console.log('New websocket connection');
+    ws.send('Welcome to the websocket server!');
+
+    ws.on('message', (message) => {
+        console.log('recieve: %s', message);
+
+        ws.send('Received message: ' + message);
+    });
+
+    ws.on('close', () => {
+        console.log('Websocket connection closed');
+    });
+};
+
+module.exports = { handleConnection };
--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const WebSocket = require('ws');
 const sqlite3 = require('sqlite3').verbose();
-const cors = require('cors');
-const WebSockets = require('ws');
 const productRoutes = require('./routes/productsRoutes')
 const orderRoutes = require('./routes/orderRoutes.js');
 const cartRoutes = require('./routes/cartRoutes')
-
+const { handleConnection } = require('./controllers/wsController');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -18,20 +17,16 @@ app.get('/', (req, res) => {
     res.send('Server is up!')
 });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 });
 
 const wss = new WebSocket.Server({ server });
 
-wss.on('connection', function connection(ws) {
-    ws.on('message', function incoming(data) {
-        console.log('received message: %s', data);
-    });
+wss.on('connection', function connection(ws, req) {
+    handleConnection(ws, req);
 });
-
 
 app.use('/', productRoutes);
 app.use('/', orderRoutes);
 app.use('/', cartRoutes);
-
